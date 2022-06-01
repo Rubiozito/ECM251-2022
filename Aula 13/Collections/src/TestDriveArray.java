c52185e94c254fb955243be1e762f9177e006892
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-06-01 11:20:20 -0300

    ok

---
+++ Aula 13/Collections/src/TestDriveArray.java
@@ -0,0 +1,11 @@
+public class TestDriveArray {
+    public static void main(String[] args) throws Exception {
+        int[] meuArray;
+        meuArray = new int[4];
+        meuArray[0] = 45;
+        meuArray[1] = 12;
+        meuArray[2] = 9;
+        meuArray[3] = 5;
+
+    }
+}
