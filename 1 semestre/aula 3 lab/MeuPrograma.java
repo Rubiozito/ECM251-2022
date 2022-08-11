c715d755ff5c3c0ce53568ed1be5ce8887987773
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-08-11 10:37:00 -0300

    ok

---
+++ 1 semestre/aula 3 lab/MeuPrograma.java
@@ -0,0 +1,8 @@
+class MeuPrograma {
+	public static void main(String[] args){
+
+		// Mensagem de saida do sistema 
+		System.out.println("minha primeira aplicação java!!");
+
+		}
+	}
--- aula 3 lab/MeuPrograma.java
+++
@@ -1,8 +0,0 @@
-class MeuPrograma {
-	public static void main(String[] args){
-
-		// Mensagem de saida do sistema 
-		System.out.println("minha primeira aplicação java!!");
-
-		}
-	}
